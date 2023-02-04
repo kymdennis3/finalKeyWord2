cbc2e3fffb1237d21734a478b44c8fb1d3c0d2c0
Kimtai <kymdennis3@gmail.com>
2023-02-04 20:56:39 +0300

    r

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        final double pi = 3.14;
+
+        System.out.println(pi);
     }
 }
